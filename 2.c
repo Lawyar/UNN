f6a3b52267216ea9077187b2210c0ebf3ff78de6
Lawyar <60302993+Lawyar@users.noreply.github.com>
2020-10-03 14:08:09 +0300

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int foruser = 0, forpc = 0, user_i = -1, pc_i = -1, attempts = 0, pc_min = 0, pc_max = 1001;;
+	char choose = '0', sign = '0';
+	printf("Choose the mode: ");
+	scanf("%c", &choose);
+	switch (choose) {
+	case '1':
+		foruser = rand() % 1001;
+		while (user_i != foruser) {
+			printf("Enter the number: ");
+			scanf("%d", &user_i);
+			if (user_i < foruser)
+				printf("Guessed number is bigger");
+			if (user_i > foruser)
+				printf("Guessed number is less");
+		}
+		printf("You win! Attempts: %d\n", attempts);
+		return 0;
+
+	case '2':
+		printf("Enter the number to pc: ");
+		scanf("%d", &forpc);
+		while (1) {
+			attempts++;
+			pc_i = (pc_min + pc_max) / 2;
+			printf("He entered: %d. Help him: ", pc_i);
+			getchar();
+			sign = getchar();
+			switch (sign) {
+			case '>':
+				pc_min = pc_i;
+				break;
+			case '<':
+				pc_max = pc_i;
+				break;
+			case '=':
+				printf("It wins! Attempts: %d", attempts);
+				return 0;
+			}
+		}
+	}
+}
