0f7c55b2a581ade6e8bd7f5bbcb210db380cb133
Lawyar <60302993+Lawyar@users.noreply.github.com>
2020-10-07 22:45:49 +0300

    Update 2'.c

--- 2'.c
+++ 2'.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
-#include <ctype.h>
 
 int main() {
 	char x;
 	int wcount = 0, dcount = 0, wbegin = 1;
 	while ((x = getchar()) != '\n') {
-		if (isdigit(x) && wbegin == 1) {
+		if (((x > '0') && (x < '9')) && wbegin == 1) {
 			dcount++;
 			wbegin = 0;
 		}
-		if (isalpha(x) && wbegin == 1) {
+		if ((((x < 'Z') && (x > 'A')) || ((x < 'z') && (x > 'a'))) && wbegin == 1) {
 			wcount++;
 			wbegin = 0;
 		}
-		if (iscntrl(x) || isspace(x)) {
+		if (x == ' ') {
 			wbegin = 1;
 		}
 
